d25e3c5dcea11d44ac3925914fce686874ad1ac6
Abdan Zam Zam Ramadhan <rabdanzamzam96@gmail.com>
2022-02-03 01:21:32 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./components/**/*.{html,js}",
+    "./pages/**/*.{html,js}",
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
